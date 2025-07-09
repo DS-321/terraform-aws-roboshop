c8de08e621d1c1b926ce19d2baae106a45f4ba17
SharmilaD <sharmiladaram419@gmail.com>
2025-07-09 20:33:15 +0530

    roboshop component module created

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  default = "dcloudlab.site"
+}
+variable "zone_id" {
+  default = "Z104650557V08CNWBHYW"
+}
+
+variable "component" {
+  
+}
+
+variable "rule_priority" {
+  
+}
